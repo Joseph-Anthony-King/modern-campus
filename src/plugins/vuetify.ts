39bbcd9efd11a3c9916bf6009bd43a5985e4774f
Joseph-Anthony-King <Joseph.King.CA@gmail.com>
2021-09-06 17:03:57 -0700

    Update the theme

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -8,4 +8,17 @@ export default new Vuetify({
   icons: {
     iconfont: 'mdi', 
   },
+  theme: {
+    themes: {
+      light: {
+        primary: "#1976D2",
+        secondary: "#021737",
+        accent: "#82B1FF",
+        error: "#FF5252",
+        info: "#2196F3",
+        success: "#4CAF50",
+        warning: "#FFC107",
+      },
+    },
+  },
 });
